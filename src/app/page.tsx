b5b014d13fe48fd1b2e347cfb50693bf6031876b
BWMarket LLC <zahir@bankway.group>
2025-07-25 04:35:02 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -186,9 +186,12 @@ return (
           <h2 className="text-secondary-foreground mt-1 text-xl font-semibold md:text-2xl">
             Hey, I'm Dev Zahir 👋
           </h2>
-          <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
-            <span ref={typedRef} className="inline-block" />
-          </h1>
+   <span
+  ref={typedRef}
+  className="inline-block min-w-[24ch] whitespace-nowrap"
+  style={{ display: 'inline-block' }}
+/>
+
         </motion.div>
 
         <motion.div
