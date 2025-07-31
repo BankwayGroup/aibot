4e9b39f530ef9f0e9bb7345fa142a83ba0250f2c
BWMarket LLC <zahir@bankway.group>
2025-07-31 01:09:43 -0500

    Update presentation.tsx

--- src/components/presentation.tsx
+++ src/components/presentation.tsx
@@ -4,7 +4,10 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import React, { useState } from 'react';
 
-import Presentation from '../presentation'; {
+// If you want to import Presentation component elsewhere, do it there:
+// import Presentation from '../presentation';
+
+export default function Profile() {
   const [imgError, setImgError] = useState(false);
 
   const profile = {
